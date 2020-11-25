13f42c268069ebd574988f2eae7beed8eac6ae10
Jim Lambert <jlambert@hashicorp.com>
2020-11-25 17:23:48 -0500

    clean up channel usage and more docs

--- oidc/callbacks/authcode_channel.go
+++ oidc/callbacks/authcode_channel.go
@@ -7,34 +7,42 @@ import (
 	"github.com/hashicorp/probo/oidc"
 )
 
-// LoginResp is written to the callback's done channel before returning
+// LoginResp is used by AuthCodeWithChannel.  The callback writes its response
+// to the returned <-chan LoginResp.
 type LoginResp struct {
 	Token oidc.Token // Token is populated when the callback successfully exchanges the auth code.
 	Error error      // Error is populated when there's an error during the callback
 }
 
 // AuthCodeWithChannel creates an oidc authorization code callback handler which
-// communicates it's results by writing a LoginResp to its done channel. This
-// callback is a one-time use callback, since it takes a specific oidc.State as
-// a parameter which represents only one oidc authentication attempt.  Because
-// of it's one-time use case, it's most appropriate when implementing a solution
-// that invokes a localhost http listener within the same process that kicked
+// communicates results by writing a LoginResp to a cannel. This callback is a
+// one-time use callback, since it takes a specific oidc.State as a parameter
+// which represents only one oidc authentication attempt.  Because of it's
+// one-time use case, it's most appropriate when implementing a solution that
+// invokes a localhost http listener within the same process that kicked
 // off the authorization code flow.  (see the vault jwt plugin as an example)
-func AuthCodeWithChannel(ctx context.Context, p *oidc.AuthCodeProvider, state oidc.State, successResponseFunc SuccessResponseFunc, errorResponsefunc ErrorResponseFunc, doneCh chan<- LoginResp) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
+//
+// The SuccessResponseFunc is used to create a response when callback is
+// successful. The ErrorResponseFunc is to create a response when the callback
+// fails.
+func AuthCodeWithChannel(ctx context.Context, p *oidc.AuthCodeProvider, state oidc.State, sFn SuccessResponseFunc, eFn ErrorResponseFunc) (<-chan LoginResp, http.HandlerFunc) {
+	doneCh := make(chan LoginResp)
+	return doneCh, func(w http.ResponseWriter, req *http.Request) {
 		const op = "callbacks.AuthCodeChannel"
-		var response string
+		var response []byte
 		var responseToken *oidc.Token
 		var responseErr error
 
 		defer func() {
-			_, _ = w.Write([]byte(response))
+			_, _ = w.Write(response)
 			doneCh <- LoginResp{*responseToken, responseErr}
+			close(doneCh)
 		}()
 
+		reqState := req.FormValue("state")
 		if state.IsExpired() {
 			responseErr = oidc.NewError(oidc.ErrExpiredState, oidc.WithOp(op), oidc.WithKind(oidc.ErrParameterViolation), oidc.WithMsg("authentication state is expired"))
-			response = errorResponsefunc(nil, responseErr)
+			response = eFn(reqState, nil, responseErr)
 			return
 		}
 		if err := req.FormValue("error"); err != "" {
@@ -46,26 +54,25 @@ func AuthCodeWithChannel(ctx context.Context, p *oidc.AuthCodeProvider, state oi
 				Uri:         req.FormValue("error_uri"),
 			}
 			responseErr = oidc.NewError(oidc.ErrLoginFailed, oidc.WithOp(op), oidc.WithKind(oidc.ErrLoginViolation), oidc.WithMsg("User failed to complete authentication/authorization"))
-			response = errorResponsefunc(reqError, responseErr)
+			response = eFn(reqState, reqError, responseErr)
 			return
 		}
 
 		// get parameters from either the body or query parameters.
 		// FormValue prioritizes body values, if found.
 		reqCode := req.FormValue("code")
-		reqState := req.FormValue("state")
 
 		if reqState != state.Id {
 			responseErr = oidc.NewError(oidc.ErrResponseStateInvalid, oidc.WithOp(op), oidc.WithKind(oidc.ErrParameterViolation), oidc.WithMsg("authen state and response state are not equal"))
-			response = errorResponsefunc(nil, responseErr)
+			response = eFn(reqState, nil, responseErr)
 			return
 		}
 		responseToken, err := p.Exchange(ctx, state, reqState, reqCode)
 		if err != nil {
 			responseErr = oidc.WrapError(err, oidc.WithOp(op), oidc.WithKind(oidc.ErrInternal), oidc.WithMsg("unable to exchange authorization code"))
-			response = errorResponsefunc(nil, err)
+			response = eFn(reqState, nil, err)
 			return
 		}
-		response = successResponseFunc(*responseToken)
+		response = sFn(reqState, *responseToken)
 	}
 }
