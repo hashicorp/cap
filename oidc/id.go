4947138881bfb9bad3e36f5e4e6b5917e611a0b0
Jim Lambert <jlambert@hashicorp.com>
2020-11-26 10:30:31 -0500

    refactored to new home

---
+++ oidc/id.go
@@ -0,0 +1,13 @@
+package oidc
+
+import "github.com/hashicorp/probo/sdk/id"
+
+// NewId generates a ID with an optional prefix.   The ID generated is suitable
+// for an State Id or Nonce
+func NewId(optionalPrefix string) (string, error) {
+	id, err := id.New(optionalPrefix)
+	if err != nil {
+		return "", NewError(ErrIdGeneratorFailed, WithOp("oidc.NewId"), WithKind(ErrInternal), WithMsg("unable to generate id"), WithWrap(err))
+	}
+	return id, nil
+}
