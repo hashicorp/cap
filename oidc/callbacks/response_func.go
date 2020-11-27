86cd20828a6d59d8740fead1b88318ddb3f0d56e
Jim Lambert <jlambert@hashicorp.com>
2020-11-27 12:55:19 -0500

    update func parameters and improve docs

--- oidc/callbacks/response_func.go
+++ oidc/callbacks/response_func.go
@@ -1,6 +1,10 @@
 package callbacks
 
-import "github.com/hashicorp/cap/oidc"
+import (
+	"net/http"
+
+	"github.com/hashicorp/cap/oidc"
+)
 
 // AuthenErrorResponse represents Oauth2 error responses.  See:
 // https://openid.net/specs/openid-connect-core-1_0.html#AuthError
@@ -10,19 +14,19 @@ type AuthenErrorResponse struct {
 	Uri         string
 }
 
-// SuccessResponseFunc is used by Callbacks to create a response when the
+// SuccessResponseFunc is used by Callbacks to create a http response when the
 // authorization code callback is successful.
 //
 // The function stateId parameter will contain the state that was returned as
-// part of a successful oidc  authentication response. The function must return
-// a []byte containing whatever context (html, JSON, etc) it would like to
-// consume with it's own http.Handler wrapper or whatever content it simply
-// wishes to send back to the user.
+// part of a successful oidc  authentication response. The oidc.Token is the
+// result of a successful token exchange with the provider.  The function
+// should use the http.ResponseWriter to send back whatever content (headers,
+// html, JSON, etc) it wishes to the client that originated the oidc flow.
 //
 // Just a reminder that the function parameters could also be used to
 // update the oidc.State for the request or log info about the request, if the
 // implementation requires it.
-type SuccessResponseFunc func(stateId string, t oidc.Token) []byte
+type SuccessResponseFunc func(stateId string, t oidc.Token, w http.ResponseWriter)
 
 // ErrorResponseFunc is used by Callbacks to create a response when the
 // authorization code callback fails.
@@ -30,11 +34,10 @@ type SuccessResponseFunc func(stateId string, t oidc.Token) []byte
 // The function receives the stateId returned as part of the oidc authentication
 // response.  It also gets parameters for the oidc authentication error response
 // and/or the callback error raised while processing the request.  The function
-// must return a []byte containing whatever context (html, JSON, etc) it would
-// like to consume with it's own http.Handler wrapper or whatever content it
-// simply wishes to send back to the user.
+// should use the http.ResponseWriter to send back whatever content (headers,
+// html, JSON, etc) it wishes to the client that originated the oidc flow.
 //
 // Just a reminder that the function parameters could also be used to
 // update the oidc.State for the request or log info about the request, if the
 // implementation requires it.
-type ErrorResponseFunc func(stateId string, r *AuthenErrorResponse, e error) []byte
+type ErrorResponseFunc func(stateId string, r *AuthenErrorResponse, e error, w http.ResponseWriter)
