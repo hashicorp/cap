bd01bbcf36f7a24774559d8d9a674c5d0407c645
Jim Lambert <jlambert@hashicorp.com>
2020-11-28 10:08:04 -0500

    moved State to it's own source file

---
+++ oidc/callback/state.go
@@ -0,0 +1,29 @@
+package callback
+
+import (
+	"context"
+
+	"github.com/hashicorp/cap/oidc"
+)
+
+// StateReader defines an interface for finding and reading an oidc.State
+// Implementions must be concurrently safe, since the reader will likely be used
+// within a concurrent http.Handler
+type StateReader interface {
+	// Read an existing AuthCodeState entry.  The returned state's Id()
+	// must match the stateId used to look it up. Implementions must be
+	// concurrently safe, which likely means returning a deep copy
+	Read(ctx context.Context, stateId string) (oidc.State, error)
+}
+
+// SingleStateReader implements the StateReader interface for a single state.
+// When it's Read() receiver function is called it will always return the same
+// state.
+type SingleStateReader struct {
+	State oidc.State
+}
+
+// Read() will always return the same state and satisfies the StateReader interface
+func (s *SingleStateReader) Read(ctx context.Context, stateId string) (oidc.State, error) {
+	return s.State, nil
+}
