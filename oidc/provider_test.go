e60458cebcbc0ba1af488a65ead2d943e0442ed1
Jim Lambert <jlambert@hashicorp.com>
2020-12-07 13:44:51 -0500

    start adding unit tests

--- oidc/provider_test.go
+++ oidc/provider_test.go
@@ -1,9 +1,16 @@
 package oidc
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/coreos/go-oidc"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestWithImplicitFlow(t *testing.T) {
@@ -18,4 +25,254 @@ func TestWithImplicitFlow(t *testing.T) {
 	testOpts = providerDefaults()
 	testOpts.withImplicitFlow = &implicitFlow{withoutAccessToken: true}
 	assert.Equal(opts, testOpts)
+}
+
+// TestNewProvider does not repeat all the Config unit tests.  It just focuses
+// on the additional tests that are unique to creating a new provider.
+func TestNewProvider(t *testing.T) {
+	t.Parallel()
+	tp := StartTestProvider(t)
+	clientId := "test-client-id"
+	clientSecret := "test-client-secret"
+	redirect := "test-redirect"
+	tests := []struct {
+		name      string
+		config    *Config
+		wantErr   bool
+		wantIsErr error
+	}{
+		{
+			name:   "valid",
+			config: testNewConfig(t, clientId, clientSecret, redirect, tp),
+		},
+		{
+			name:      "nil-config",
+			config:    nil,
+			wantErr:   true,
+			wantIsErr: ErrNilParameter,
+		},
+		{
+			name: "invalid-config",
+			config: func() *Config {
+				c := testNewConfig(t, clientId, clientSecret, redirect, tp)
+				c.Issuer = ""
+				return c
+			}(),
+			wantErr:   true,
+			wantIsErr: ErrInvalidParameter,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := NewProvider(tt.config)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Truef(errors.Is(err, tt.wantIsErr), "wanted \"%s\" but got \"%s\"", tt.wantIsErr, err)
+				return
+			}
+			require.NoError(err)
+			assert.NotNil(got.config)
+			assert.NotNil(got.provider)
+			assert.NotNil(got.client)
+			assert.NotNil(got.backgroundCtx)
+			assert.NotNil(got.backgroundCtxCancel)
+		})
+	}
+}
+
+func TestProvider_Done(t *testing.T) {
+	t.Parallel()
+	tp := StartTestProvider(t)
+	p := testNewProvider(t, "client-id", "client-secret", "redirect", tp)
+
+	tests := []struct {
+		name                string
+		provider            *oidc.Provider
+		client              *http.Client
+		backgroundCtx       context.Context
+		backgroundCtxCancel context.CancelFunc
+	}{
+		{
+			name:                "all-valid",
+			provider:            p.provider,
+			client:              p.client,
+			backgroundCtx:       p.backgroundCtx,
+			backgroundCtxCancel: p.backgroundCtxCancel,
+		},
+		{
+			name:                "nil-provider",
+			provider:            nil,
+			client:              p.client,
+			backgroundCtx:       p.backgroundCtx,
+			backgroundCtxCancel: p.backgroundCtxCancel,
+		},
+		{
+			name:                "nil-client",
+			provider:            p.provider,
+			client:              nil,
+			backgroundCtx:       p.backgroundCtx,
+			backgroundCtxCancel: p.backgroundCtxCancel,
+		},
+		{
+			name:                "nil-backgroundCtx",
+			provider:            p.provider,
+			client:              p.client,
+			backgroundCtx:       p.backgroundCtx,
+			backgroundCtxCancel: nil,
+		},
+		{
+			name:                "nil-backgroundCtxCancel",
+			provider:            p.provider,
+			client:              p.client,
+			backgroundCtx:       p.backgroundCtx,
+			backgroundCtxCancel: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Provider{
+				provider:            tt.provider,
+				client:              tt.client,
+				backgroundCtx:       tt.backgroundCtx,
+				backgroundCtxCancel: tt.backgroundCtxCancel,
+			}
+			p.Done()
+		})
+	}
+	t.Run("nil-provider", func(t *testing.T) {
+		var p *Provider
+		p.Done()
+	})
+}
+
+func TestProvider_AuthURL(t *testing.T) {
+	ctx := context.Background()
+	clientId := "test-client-id"
+	clientSecret := "test-client-secret"
+	redirect := "test-redirect"
+	tp := StartTestProvider(t)
+	p := testNewProvider(t, clientId, clientSecret, redirect, tp)
+	validState, err := NewState(1 * time.Second)
+	require.NoError(t, err)
+
+	type args struct {
+		ctx context.Context
+		s   State
+		opt []Option
+	}
+	tests := []struct {
+		name      string
+		p         *Provider
+		args      args
+		wantUrl   string
+		wantErr   bool
+		wantIsErr error
+	}{
+		{
+			name: "valid-using-default-auth-flow",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s:   validState,
+			},
+			wantUrl: func() string {
+				return fmt.Sprintf(
+					"%s/authorize?client_id=%s&nonce=%s&redirect_uri=%s&response_type=code&scope=openid&state=%s",
+					tp.Addr(),
+					clientId,
+					validState.Nonce(),
+					redirect,
+					validState.Id(),
+				)
+			}(),
+		},
+		{
+			name: "valid-using-implicit-flow",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s:   validState,
+				opt: []Option{WithImplicitFlow()},
+			},
+			wantUrl: func() string {
+				return fmt.Sprintf(
+					"%s/authorize?client_id=%s&nonce=%s&redirect_uri=%s&response_mode=form_post&response_type=id_token+token&scope=openid&state=%s",
+					tp.Addr(),
+					clientId,
+					validState.Nonce(),
+					redirect,
+					validState.Id(),
+				)
+			}(),
+		},
+		{
+			name: "valid-using-implicit-flow-no-access-token",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s:   validState,
+				opt: []Option{WithImplicitFlow(true)},
+			},
+			wantUrl: func() string {
+				return fmt.Sprintf(
+					"%s/authorize?client_id=%s&nonce=%s&redirect_uri=%s&response_mode=form_post&response_type=id_token&scope=openid&state=%s",
+					tp.Addr(),
+					clientId,
+					validState.Nonce(),
+					redirect,
+					validState.Id(),
+				)
+			}(),
+		},
+		{
+			name: "empty-state-nonce",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s: &St{
+					id: "s_1234567890",
+				},
+			},
+			wantErr:   true,
+			wantIsErr: ErrInvalidParameter,
+		},
+		{
+			name: "empty-state-id",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s: &St{
+					nonce: "s_1234567890",
+				},
+			},
+			wantErr:   true,
+			wantIsErr: ErrInvalidParameter,
+		},
+		{
+			name: "equal-state-id-and-nonce",
+			p:    p,
+			args: args{
+				ctx: ctx,
+				s: &St{
+					id:    "s_1234567890",
+					nonce: "s_1234567890",
+				},
+			},
+			wantErr:   true,
+			wantIsErr: ErrInvalidParameter,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			gotUrl, err := tt.p.AuthURL(tt.args.ctx, tt.args.s, tt.args.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Truef(errors.Is(err, tt.wantIsErr), "wanted \"%s\" but got \"%s\"", tt.wantIsErr, err)
+				return
+			}
+			require.Equalf(tt.wantUrl, gotUrl, "Provider.AuthURL() = %v, want %v", gotUrl, tt.wantUrl)
+		})
+	}
 }
