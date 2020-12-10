6ae5259b42c569977750e396c3b558c80e326cb7
Michael Gaffney <mike@gaffney.cc>
2020-12-10 09:34:08 -0500

    Add package level doc

---
+++ oidc/doc.go
@@ -0,0 +1,4 @@
+// Package oidc provides support for making authentication requests as
+// specified in OpenID Connect Core 1.0. It supports the Authorization Code
+// Flow and the Implicit Flow.
+package oidc
