e193aa08bc840904082c72d4dcc59facfa372322
Jim Lambert <jlambert@hashicorp.com>
2020-12-11 09:26:45 -0500

    needed RemoveDuplicatesStable

--- oidc/internal/strutils/strutils_test.go
+++ oidc/internal/strutils/strutils_test.go
@@ -1,6 +1,7 @@
 package strutils
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,4 +18,31 @@ func TestStrutil_ListContains(t *testing.T) {
 	}
 	require.False(StrListContains(haystack, "tubez"))
 	require.True(StrListContains(haystack, "root"))
+}
+
+func TestStrUtil_RemoveDuplicatesStable(t *testing.T) {
+	type tCase struct {
+		input           []string
+		expect          []string
+		caseInsensitive bool
+	}
+
+	tCases := []tCase{
+		{[]string{}, []string{}, false},
+		{[]string{}, []string{}, true},
+		{[]string{"a", "b", "a"}, []string{"a", "b"}, false},
+		{[]string{"A", "b", "a"}, []string{"A", "b", "a"}, false},
+		{[]string{"A", "b", "a"}, []string{"A", "b"}, true},
+		{[]string{" ", "d", "c", "d"}, []string{"d", "c"}, false},
+		{[]string{"Z ", " z", " z ", "y"}, []string{"Z ", "y"}, true},
+		{[]string{"Z ", " z", " z ", "y"}, []string{"Z ", " z", "y"}, false},
+	}
+
+	for _, tc := range tCases {
+		actual := RemoveDuplicatesStable(tc.input, tc.caseInsensitive)
+
+		if !reflect.DeepEqual(actual, tc.expect) {
+			t.Fatalf("Bad testcase %#v, expected %v, got %v", tc, tc.expect, actual)
+		}
+	}
 }
