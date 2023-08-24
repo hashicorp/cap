512b26a36f7977f99783bf858ce24263cad65d17
Jim <jlambert@hashicorp.com>
2023-08-24 15:52:46 -0400

    tests (ldap): add fuzz test for ldap.escapeValue(...) (#88)

--- ldap/conn_test.go
+++ ldap/conn_test.go
@@ -7,28 +7,38 @@ import (
 	"testing"
 )
 
-func Test_EscapeValue(t *testing.T) {
-	testcases := map[string]string{
-		"#test":                      "\\#test",
-		"test,hello":                 "test\\,hello",
-		"test,hel+lo":                "test\\,hel\\+lo",
-		"test\\hello":                "test\\\\hello",
-		"  test  ":                   "\\  test \\ ",
-		"":                           "",
-		`\`:                          `\\`,
-		"trailing\000":               `trailing\00`,
-		"mid\000dle":                 `mid\00dle`,
-		"\000":                       `\00`,
-		"multiple\000\000":           `multiple\00\00`,
-		"backlash-before-null\\\000": `backlash-before-null\\\00`,
-		"trailing\\":                 `trailing\\`,
-		"double-escaping\\>":         `double-escaping\\\>`,
-	}
+var testcases = map[string]string{
+	"#test":                      "\\#test",
+	"test,hello":                 "test\\,hello",
+	"test,hel+lo":                "test\\,hel\\+lo",
+	"test\\hello":                "test\\\\hello",
+	"  test  ":                   "\\  test \\ ",
+	"":                           "",
+	`\`:                          `\\`,
+	"trailing\000":               `trailing\00`,
+	"mid\000dle":                 `mid\00dle`,
+	"\000":                       `\00`,
+	"multiple\000\000":           `multiple\00\00`,
+	"backlash-before-null\\\000": `backlash-before-null\\\00`,
+	"trailing\\":                 `trailing\\`,
+	"double-escaping\\>":         `double-escaping\\\>`,
+}
 
+func Test_EscapeValue(t *testing.T) {
 	for test, answer := range testcases {
 		res := escapeValue(test)
 		if res != answer {
 			t.Errorf("Failed to escape %q: %q != %q\n", test, res, answer)
 		}
 	}
+}
+
+// Fuzz_EscapeValue is only focused on finding panics
+func Fuzz_EscapeValue(f *testing.F) {
+	for tc, _ := range testcases {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, s string) {
+		_ = escapeValue(s)
+	})
 }
