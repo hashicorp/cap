26c0e39a4c890499b6810ccb8c530e98225101be
Jim <jlambert@hashicorp.com>
2023-09-09 12:41:14 -0400

    fix (saml): address possible panics in saml handlers

--- saml/handler/metadata.go
+++ saml/handler/metadata.go
@@ -2,17 +2,25 @@ package handler
 
 import (
 	"encoding/xml"
+	"fmt"
 	"net/http"
 
 	"github.com/hashicorp/cap/saml"
 )
 
-func MetadataHandlerFunc(sp *saml.ServiceProvider) http.HandlerFunc {
+// MetadataHandlerFunc creates a handler function that handles a SAML
+// metadata request
+func MetadataHandlerFunc(sp *saml.ServiceProvider) (http.HandlerFunc, error) {
+	const op = "handler.MetadataHandlerFunc"
+	switch {
+	case sp == nil:
+		return nil, fmt.Errorf("%s: missing service provider", op)
+	}
 	return func(w http.ResponseWriter, _ *http.Request) {
 		meta := sp.CreateMetadata()
 		err := xml.NewEncoder(w).Encode(meta)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-	}
+	}, nil
 }
