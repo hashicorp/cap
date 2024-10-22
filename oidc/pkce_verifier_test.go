4ee9df57cdb5d4b9cb1a96bf22eda071398c8435
Jim <jlambert@hashicorp.com>
2024-10-22 09:33:50 -0400

    feat (oidc): add WithVerifier
    This commit adds the WithVerifier option to the
    oidc package. This option allows the user to
    specify a custom verifier for the PKCE code
    challenge.

--- oidc/pkce_verifier_test.go
+++ oidc/pkce_verifier_test.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/hashicorp/cap/oidc/internal/base62"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -18,7 +19,7 @@ func TestNewCodeVerifier(t *testing.T) {
 		assert, require := assert.New(t), require.New(t)
 		got, err := NewCodeVerifier()
 		require.NoError(err)
-		assert.Equal(verifierLen, len(got.verifier))
+		assert.Equal(minVerifierLen, len(got.verifier))
 		assert.Equal(S256, got.Method())
 
 		challenge, err := CreateCodeChallenge(got)
@@ -52,4 +53,36 @@ func TestCreateCodeChallenge(t *testing.T) {
 		assert.Empty(challenge)
 		assert.True(errors.Is(err, ErrUnsupportedChallengeMethod))
 	})
+}
+
+func Test_WithVerifier(t *testing.T) {
+	t.Parallel()
+	assert, require := assert.New(t), require.New(t)
+	v, err := base62.Random(43)
+	require.NoError(err)
+	got, err := NewCodeVerifier(WithVerifier(v))
+	require.NoError(err)
+	assert.Equal(v, got.Verifier())
+
+	// Test that the verifier is too short
+	v, err = base62.Random(42)
+	require.NoError(err)
+	_, err = NewCodeVerifier(WithVerifier(v))
+	require.Error(err)
+	assert.Contains(err.Error(), "verifier length is less than 43")
+
+	// Test that the verifier is too long
+	v, err = base62.Random(129)
+	require.NoError(err)
+	_, err = NewCodeVerifier(WithVerifier(v))
+	require.Error(err)
+	assert.Contains(err.Error(), "verifier length is greater than 128")
+
+	// Test that the verifier contains invalid characters
+	v, err = base62.Random(43)
+	require.NoError(err)
+	v = v + "!"
+	_, err = NewCodeVerifier(WithVerifier(v))
+	require.Error(err)
+	assert.Contains(err.Error(), "verifier contains invalid characters")
 }
