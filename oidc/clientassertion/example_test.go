8f9aa91bb42e49f4f298cbf80a61201e771c98f0
Daniel Bennett <dbennett@hashicorp.com>
2025-02-26 13:18:23 -0500

    add ExampleJWT test

---
+++ oidc/clientassertion/example_test.go
@@ -0,0 +1,99 @@
+package clientassertion
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"log"
+
+	"github.com/go-jose/go-jose/v4"
+	"github.com/go-jose/go-jose/v4/jwt"
+)
+
+func ExampleJWT() {
+	// With an HMAC client secret
+	secret := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 32 bytes for HS256
+	j, err := NewJWT("client-id", []string{"audience"},
+		WithClientSecret(secret, HS256),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	signed, err := j.Serialize()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	{
+		// decode and inspect the JWT -- this is the IDP's job,
+		// but it illustrates the example.
+		token, err := jwt.ParseSigned(signed, []jose.SignatureAlgorithm{"HS256"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		headers := token.Headers[0]
+		fmt.Printf("ClientSecret\n  Headers - Algorithm: %s; typ: %s\n",
+			headers.Algorithm, headers.ExtraHeaders["typ"])
+		var claim jwt.Claims
+		err = token.Claims([]byte(secret), &claim)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("  Claims  - Issuer: %s; Subject: %s; Audience: %v\n",
+			claim.Issuer, claim.Subject, claim.Audience)
+	}
+
+	// With an RSA key
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pubKey, ok := privKey.Public().(*rsa.PublicKey)
+	if !ok {
+		log.Fatal("couldn't get rsa.PublicKey from PrivateKey")
+	}
+	j, err = NewJWT("client-id", []string{"audience"},
+		WithRSAKey(privKey, RS256),
+		// note: for some providers, they key ID may be an x5t derivation
+		// of a cert generated from the private key.
+		// if your key has an associated JWKS endpoint, it will be the "kid"
+		// for the public key at /.well-known/jwks.json
+		WithKeyID("some-key-id"),
+		// extra headers, like x5t, are optional
+		WithHeaders(map[string]string{
+			"x5t": "should-be-derived-from-a-cert",
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	signed, err = j.Serialize()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	{ // decode and inspect the JWT -- this is the IDP's job
+		token, err := jwt.ParseSigned(signed, []jose.SignatureAlgorithm{"RS256"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		h := token.Headers[0]
+		fmt.Printf("PrivateKey\n  Headers - KeyID: %s; Algorithm: %s; typ: %s; x5t: %s\n",
+			h.KeyID, h.Algorithm, h.ExtraHeaders["typ"], h.ExtraHeaders["x5t"])
+		var claim jwt.Claims
+		err = token.Claims(pubKey, &claim)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("  Claims  - Issuer: %s; Subject: %s; Audience: %v\n",
+			claim.Issuer, claim.Subject, claim.Audience)
+	}
+
+	// Output:
+	// ClientSecret
+	//   Headers - Algorithm: HS256; typ: JWT
+	//   Claims  - Issuer: client-id; Subject: client-id; Audience: [audience]
+	// PrivateKey
+	//   Headers - KeyID: some-key-id; Algorithm: RS256; typ: JWT; x5t: should-be-derived-from-a-cert
+	//   Claims  - Issuer: client-id; Subject: client-id; Audience: [audience]
+}
