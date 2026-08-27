4415c37bdad18ecae2ab682aa78221226b105f55
Ryan Derr <ryan.derr@hashicorp.com>
2026-08-27 11:45:59 -0500

    feat(oidc): Creation Of New Provider Type Option For Provider Config

---
+++ oidc/provider_type.go
@@ -0,0 +1,23 @@
+// Copyright IBM Corp. 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package oidc
+
+// ProviderType identifies a given known OIDC provider.
+type ProviderType string
+
+const (
+	// ProviderTypeAzure is the provider identifier for Microsoft Azure.
+	ProviderTypeAzure ProviderType = "azure"
+)
+
+// supportedProviderTypes is a map of known OIDC provider types.
+var supportedProviderTypes = map[ProviderType]struct{}{
+	ProviderTypeAzure: {},
+}
+
+// SupportedProviderType returns true if the given provider type is known and supported.
+func SupportedProviderType(p ProviderType) bool {
+	_, ok := supportedProviderTypes[p]
+	return ok
+}
